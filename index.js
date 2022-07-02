dff325c6783a9e6d37ac501eccfb5395fe1574e5
mahmudunnaby <mahmudunnabyemon0@gmail.com>
2022-07-02 14:49:05 +0600

    Delete  api created

--- index.js
+++ index.js
@@ -62,6 +62,17 @@ async function run() {
             res.send(result)
         })
 
+        //delete a product
+
+        app.delete('/delete-billing/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+
+            const result = await billCollection.deleteOne(query);
+            res.send(result)
+        })
+
 
     }
 
